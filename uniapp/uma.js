9425bdaf603d0ad84e058e3e24c9649addf4bede
云猫🐱 <zhihao.pzh@alibaba-inc.com>
2020-07-15 20:30:04 +0800

    更新uni-app 字节demo

--- uniapp/uma.js
+++ uniapp/uma.js
@@ -16,13 +16,13 @@ uma.init({
 // #endif
 
 // #ifdef MP-TOUTIAO
-import uma from './libs/umtrack-tt/uma.min.js'
+import uma from './libs/umtrack-tt/uma.min.js';
 uma.init({
-    appKey: 'YOUR_UMENG_APPKEY', //由友盟分配的APP_KEY
-    autoGetOpenid: false, // 是否需要通过友盟后台获取openid或匿名openid，如若需要，请到友盟后台设置appId及secret
+    appKey:'yourappkey', //由友盟分配的APP_KEY
+    autoGetOpenid: true, // 是否需要通过友盟后台获取openid或匿名openid，如若需要，请到友盟后台设置appId及secret
     debug: true, //是否打开调试模式
-    uploadUserInfo: false // 自动上传用户信息，设为false取消上传，默认为false
-  })
+    uploadUserInfo: true // 自动上传用户信息，设为false取消上传，默认为false
+ })
 // #endif
 
 // #ifdef H5
