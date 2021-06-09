e55f507ff9a4e155372c12dd1a918b919551a0b8
云猫🐱 <zhihao.pzh@alibaba-inc.com>
2021-06-09 14:14:39 +0800

    微信小游戏sdk回归demo

--- wx-game/game.js
+++ wx-game/game.js
@@ -4,3 +4,40 @@ import './js/umstat';
 import Main from './js/main'
 
 new Main()
+wx.onShow(()=>{
+  wx.getSetting({
+    success(res) {
+      if (!res.authSetting['scope.userInfo']) {
+        wx.authorize({
+          scope: 'scope.userInfo',
+          success () {
+            // 用户已经同意小程序使用录音功能，后续调用 wx.startRecord 接口不会弹窗询问
+            
+          },
+          fail:function(){
+            wx.openSetting({
+              withSubscriptions: true,
+            })
+            let button = wx.createOpenSettingButton({
+              type: 'text',
+              text: '打开设置页面',
+              style: {
+                left: 10,
+                top: 76,  
+                width: 200,
+                height: 40,
+                lineHeight: 40,
+                backgroundColor: '#ff0000',
+                color: '#ffffff',
+                textAlign: 'center',
+                fontSize: 16,
+                borderRadius: 4
+              }
+            })
+          }
+        })
+      }
+    }
+  })
+
+})
--- wx-game/js/utils/umtrack-wx-game/index.js
+++ wx-game/js/utils/umtrack-wx-game/index.js
@@ -4,6 +4,8 @@ uma.init({
   appKey: 'YOUR APP KEY',
   useOpenid: false, //default true
   autoGetOpenid: false,
-  debug: true
+  debug: true,
+  uploadUserInfo:true
 });
+//uma.setUserInfo({city:88})
 export default uma;
